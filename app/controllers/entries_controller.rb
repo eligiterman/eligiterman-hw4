5c55ac8f0adedbb630df87964ec18b0e783a1463
Eli Giterman <eli.giterman@kellogg.northwestern.edu>
2025-05-31 17:16:18 +0000

    Final draft complete

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,24 +1,24 @@
 class EntriesController < ApplicationController
 
   def new
+  end
+
+  def create
     @user = User.find_by({ "id" => session["user_id"] })
     if @user != nil
       @entry = Entry.new
+      @entry["title"] = params["title"]
+      @entry["description"] = params["description"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry["image"] = params["image"]
+      @entry["place_id"] = params["place_id"]
+      @entry["user_id"] = session["user_id"]
+      @entry.save
+      redirect_to "/places/#{@entry["place_id"]}"
     else
       flash["notice"] = "You must log in to add a journal entry."
       redirect_to "/login"
     end
-  end
-
-  def create
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry["user_id"] = session["user_id"]
-    @entry.save
-    redirect_to "/places/#{@entry["place_id"]}"
   end
 
 end
